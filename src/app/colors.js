e6e58ca906f2d9f6bf55abd3519e02f34d069f50
Anna Orlova <Anny412@yandex.ru>
2018-11-29 17:20:02 +0200

    color track configuration

--- src/app/colors.js
+++ src/app/colors.js
@@ -22,16 +22,16 @@ export default {
         hex: 0x00FF00,
         colorFunction: function(r, g, b) {
             return (
-                r > 70 && r < 140 &&
-                g > 160 && g < 210 &&
-                b > 70 && b < 115
+                r > 75 && r < 125 &&
+                g > 115 && g < 155 &&
+                b > 55 && b < 110
             )
         }
     },
     'red': {
         hex: 0xFF0000,
         colorFunction: function(r, g, b) {
-            return r > 200 && g < 50 && b < 50
+            return r > 190 && g < 110 && b < 95
         }
     }
 }
